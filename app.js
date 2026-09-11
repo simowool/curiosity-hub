aaa88c4ebd0d20518c803001a7184274e1ac9a15
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 08:15:16 +0000

    Bump data.json cache-bust after tag cleanup

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911q")
+fetch("data.json?v=20260911r")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
