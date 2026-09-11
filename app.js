fdd38b75f9c8c6422f2e631fa363f51139032471
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 07:28:30 +0000

    Bump data.json cache-bust for NASA Kids Club

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911m")
+fetch("data.json?v=20260911n")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
