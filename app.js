fbb5c5d7b51693c2154aafc5534e4bcab8df0b13
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 07:17:13 +0000

    Bump data.json cache-bust for BBC Bitesize

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911k")
+fetch("data.json?v=20260911l")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
