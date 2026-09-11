8463573c48867f942d8779f9a44228ac0a946526
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 09:35:26 +0000

    Bump data.json cache-bust for Code.org

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911s")
+fetch("data.json?v=20260911t")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
