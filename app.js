36ed3db2b694a52b9c2551e99587b0a6e0b924f5
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 07:14:36 +0000

    Bump data.json cache-bust for Scratch

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911j")
+fetch("data.json?v=20260911k")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
