ddebbc4c4b7f25795eb5afa74569c3b61c4efac4
simowool <64458108+simowool@users.noreply.github.com>
2026-09-11 05:48:36 +0000

    Bump data.json cache-bust for Space Place

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911f")
+fetch("data.json?v=20260911g")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
