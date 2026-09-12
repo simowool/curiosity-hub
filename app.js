0bea23b0809168a9e217e90390f4aec2030977ca
simowool <64458108+simowool@users.noreply.github.com>
2026-09-12 02:08:42 +0000

    Bump data.json cache-bust for Cool English

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function esc(s) {
     .replace(/"/g, "&quot;");
 }
 
-fetch("data.json?v=20260911v")
+fetch("data.json?v=20260912a")
   .then(function (r) {
     if (!r.ok) throw new Error("data.json HTTP " + r.status);
     return r.json();
